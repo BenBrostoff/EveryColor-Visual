59417f2befcebf1e48374b7a4559f514a1f4d5fa
Ben Brostoff <ben.brostoff@gmail.com>
2014-09-06 18:34:08 -0400

    Max text summary added

--- EveryColor_Visual/app/assets/javascripts/gen_colors.js
+++ EveryColor_Visual/app/assets/javascripts/gen_colors.js
@@ -2,25 +2,61 @@ var Build = function(dataset) {
   var svg = d3.select("body")
               .append("svg")
 
+  var testMaxWidth = function(d) {
+    if (d[3] == 50) {
+      return 0.35 * $("svg").width();
+      }
+    else {return d[0] * $("svg").width()};
+  }
+
+  var testMaxHeight = function(d) {
+    if (d[3] == 50) {
+      return 0.55 * $("svg").height();
+      }
+    else {return d[1] * $("svg").height()};
+  }
+
   svg.selectAll("circle")
-     .data(dataset)
+     .data(dataset.colors)
      .enter()
      .append("circle")
 
      .attr("cx", function(d) {
-        console.log($("svg").width());
-        console.log($("svg").height());
-        return d[0] * $("svg").width();
+        return testMaxWidth(d);
      })
      .attr("cy", function(d) {
-        return d[1] * $("svg").height();
+       return testMaxHeight(d);
      })
      .attr("fill", function(d) {
         return d[2];
      })
      .attr("r", function(d) {
         return d[3]; 
+     });
+
+  svg.selectAll("text")
+     .data(dataset.colors)
+     .enter()
+     .append("text")
+     .text(function(d) {
+        return d[2] + " / " + dataset.max + " favs and rts" +
+                " / " + d[4];
      })
+     .attr("x", function(d) {
+        return testMaxWidth(d);
+      })
+     .attr("y", function(d) {
+        return testMaxHeight(d) + 75;
+    })
+    .attr("font-family", "sans-serif")
+    .attr("font-size", function(d) {
+        var size = d[3]/50 * 20;
+        if (size == 20) {
+          return 30;
+        }
+        else {return 0;}
+    })
+    .attr("fill", "black");
 }
 
 var Dataset = function() {
@@ -35,13 +71,11 @@ var Dataset = function() {
     });
   }
     getTheColors().done(function(result) {
-      console.log("COMPLETE")
       dataset.colors = result.colors;
-      console.log(dataset.colors);
-      Build(dataset.colors);
+      dataset.max = result.max;
+      Build(dataset);
     });
 }
-
 
  $(document).ready(function() {
     Dataset();
--- EveryColor_Visual/app/controllers/colors_controller.rb
+++ EveryColor_Visual/app/controllers/colors_controller.rb
@@ -8,12 +8,11 @@ class ColorsController < ApplicationController
     holder, max_find = [], []
     $client.user_timeline("everycolorbot", :count => 200).each do |tweet|
       max_find << tweet.favorite_count + tweet.retweet_count 
-      holder << [gen_percent, gen_percent, "#" + tweet.text.split(" ")[0][2..-1], tweet.favorite_count + tweet.retweet_count] 
+      holder << [gen_percent, gen_percent, "#" + tweet.text.split(" ")[0][2..-1], 
+                 tweet.favorite_count + tweet.retweet_count, tweet.created_at.to_s[0..9]] 
     end
-    print holder[0]
     holder.map {|tweet| tweet[3] = (tweet[3].to_f / max_find.max.to_f) * 50}
-    print holder[0]
-    render json: {colors: holder}
+    render json: {colors: holder, max: max_find.max}
   end
 
   def index
