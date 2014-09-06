0298e42087fd796881d8db1e9565d8f0e290ac55
Ben Brostoff <ben.brostoff@gmail.com>
2014-09-06 16:58:08 -0400

    Graph rendering, need to figure out colors

--- EveryColor_Visual/app/assets/javascripts/gen_colors.js
+++ EveryColor_Visual/app/assets/javascripts/gen_colors.js
@@ -1,13 +1,48 @@
-  var setColors = function(){
-    return $.ajax({
-      url : '/generate',
-      method : 'GET',
-      success : function(response){
-        console.log(response.colors)
+var Build = function(dataset) {
+  var svg = d3.select("body")
+              .append("svg")
+              .attr("width", 600)
+              .attr("height", 600);
+
+  svg.selectAll("circle")
+     .data(dataset)
+     .enter()
+     .append("circle")
+
+     .attr("cx", function(d) {
+        return d[0];
+     })
+     .attr("cy", function(d) {
+        return d[1];
+     })
+     .attr("color", function(d) {
+        return "#" + d[2];
+     })
+     .attr("r", function(d) {
+        return d[3]; 
+     })
+}
+
+var Dataset = function() {
+    dataset = {};
+    var getTheColors = function() {
+      return $.ajax({
+        url : '/generate',
+        method: 'get',
+        success: function(response) {
+          result = response;
       }
     });
-  };
+  }
+    getTheColors().done(function(result) {
+      console.log("COMPLETE")
+      dataset.colors = result.colors;
+      console.log(dataset.colors);
+      Build(dataset.colors);
+    });
+}
+
 
  $(document).ready(function() {
-  setColors();
+    Dataset();
 });
--- EveryColor_Visual/app/controllers/colors_controller.rb
+++ EveryColor_Visual/app/controllers/colors_controller.rb
@@ -1,10 +1,14 @@
 class ColorsController < ApplicationController
 
   def generate
-    holder = []
+    holder, max_find = [], []
     $client.user_timeline("everycolorbot", :count => 200).each do |tweet|
-      holder << [tweet.text.split(" ")[0], tweet.favorite_count + tweet.retweet_count] 
+      max_find << tweet.favorite_count + tweet.retweet_count 
+      holder << [rand(100), rand(100), tweet.text.split(" ")[0], tweet.favorite_count + tweet.retweet_count] 
     end
+    print holder[0]
+    holder.map {|tweet| tweet[3] = (tweet[3].to_f / max_find.max.to_f) * 6}
+    print holder[0]
     render json: {colors: holder}
   end
 
